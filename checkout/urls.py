1ab6290335059b3173abe2c1517dabb0adbf03a8
Aizan Ahmed <malikaizan198@gmail.com>
2025-06-24 15:55:58 +0500

    stripe integrated for payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('products/', ProductsListView, name='products-list'),
     path('pricing/<int:productID>/', PricingView, name='pricing'),
     path('create-checkout-session/<int:productID>/', CreateCheckoutSessionView, name='create-checkout-session'),
     path('payment-success/<int:productID>/', PaymentSuccessView, name='payment-success'),
     path('payment-failed/<int:productID>/', PaymentFailedView, name='payment-failed'),
+    path('checkout/<int:product_id>/', CreateCheckoutSessionView, name='checkout'),
 ]
